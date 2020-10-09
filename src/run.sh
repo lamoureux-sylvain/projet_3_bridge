5caad17245482cf532adcf504f48502479783eee
lamoureux-sylvain <47850225+lamoureux-sylvain@users.noreply.github.com>
2020-10-09 16:55:01 +0200

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-gunicorn src.wsgi --bind=0.0.0.0:80
+gunicorn projet_3_bridge.wsgi --bind=0.0.0.0:80
